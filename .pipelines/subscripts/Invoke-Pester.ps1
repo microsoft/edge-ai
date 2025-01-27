c72924f23cdf3cf72be60568e369b78411f31498
Bill Berry <wberry@microsoft.com>
2025-01-27 17:22:19 +0000

    feat(docs): Improve internal pipeline commenting and docs, pwsh mocks - Merged PR 80
    This PR:
    
    - improves the internal Az pipeline commenting and docs.
    - adds Pwsh mocks
    - adds Pester version install check, for performance
    
    Related work items: #579, #580

--- .pipelines/subscripts/Invoke-Pester.ps1
+++ .pipelines/subscripts/Invoke-Pester.ps1
@@ -10,13 +10,45 @@ param (
     [string]$OutputFile
 )
 
-# Update Pester to the latest version
-Install-Module -Name Pester -Force -AllowClobber
+function Find-PesterModule {
+    param (
+        [string]$ModuleName = "Pester",
+        [string]$MinimumVersion = "5.0.0"
+    )
+
+    # Check if the module is installed
+    $module = Get-Module -ListAvailable -Name $ModuleName | Sort-Object -Property Version -Descending | Select-Object -First 1
+
+    if ($null -eq $module -or $module.Version -lt [version]$MinimumVersion) {
+        Write-Host "Installing or updating $ModuleName to version $MinimumVersion or higher..."
+        Install-Module -Name $ModuleName -MinimumVersion $MinimumVersion -Force -AllowClobber
+    } else {
+        Write-Host "$ModuleName version $($module.Version) is already installed."
+    }
+}
+
+# Ensure Pester module is installed and at least version 5
+Find-PesterModule -ModuleName "Pester" -MinimumVersion "5.0.0"
+
 
 Import-Module Pester
+
+$configuration = [PesterConfiguration]@{
+    Run = @{
+        Path = $Path
+    }
+    Output = @{
+        Verbosity = 'Detailed'
+     }
+     TestResult = @{
+        Enabled = $true
+        OutputFormat = "NUnitXml"
+        OutputPath   = $OutputFile
+    }
+}
 
 # Print out the Path and OutputFile variables
 Write-Host "Test file path: $Path"
 Write-Host "Output test file path: $OutputFile"
 
-Invoke-Pester -Path $Path -OutputFile $OutputFile -OutputFormat NUnitXml
+Invoke-Pester -Configuration $configuration
