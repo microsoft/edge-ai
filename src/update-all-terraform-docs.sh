2c489500315cafa2a0f57be3e2735c56e3a63b78
Bill Berry <wberry@microsoft.com>
2025-01-29 20:10:28 +0000

    fix(build): fix tf doc gen script - Merged PR 90
    update tf doc generation script to run over correct folder names
    
    Related work items: #589

--- src/update-all-terraform-docs.sh
+++ src/update-all-terraform-docs.sh
@@ -3,30 +3,21 @@
 set -e
 
 # Find all top-level directories in the src folder that contain a nested terraform folder
-
+echo "$PWD"
 TERRAFORM_DOCS_CONFIG=".terraform-docs.yml"
 echo "Starting document processing using terraform-docs with config file: $TERRAFORM_DOCS_CONFIG"
 
-update_terraform_docs() {
-  local folder_name="$1"
-  local folder_path="$folder_name/terraform/"
-  echo "Updating README.md for terraform in $folder_path"
-  terraform-docs "$folder_path" --config $TERRAFORM_DOCS_CONFIG
-}
-
 echo "Searching top-level directories containing terraform ..."
-# Find all top-level directories in the src folder that contain a nested terraform folder
-folders=()
-# -r read literal; -d delimiter is null (spaces and special chars)
-while IFS= read -r -d '' folder; do
-  folders+=("$folder")
-# print parent directories separated by null char, exec on each find
-done < <(find src -mindepth 2 -maxdepth 2 -type d -name 'terraform' -exec dirname {} \; -print0 | sort -zu)
+echo "Starting terraform doc updates ..."
 
-echo "Starting terraform doc update for the following folders: ${folders[*]}"
-
-for folder in "${folders[@]}"; do
-  echo "Updating Terrform docs in folder: $folder"
-  update_terraform_docs "$folder"
-  echo "Updated Terrform docs in folder: $folder"
+# Loop over top_level_dirs and select only the folders 
+# where there is a "terraform" subfolder and call tf-docs on them
+find src -mindepth 1 -maxdepth 1 -type d | while read -r folder; do
+  if [ -d "$folder/terraform" ]; then
+    echo "Updating Terraform docs in folder: $folder/terraform"
+    terraform-docs "$folder/terraform" --config "$TERRAFORM_DOCS_CONFIG"
+    echo "Completed processing Terraform docs in folder: $folder"
+  fi
 done
+
+
