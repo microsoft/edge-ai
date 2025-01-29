654ae78da9caef057c95cbe3cedf7c69915e53b1
Bill Berry <wberry@microsoft.com>
2025-01-29 22:16:31 +0000

    fix(build): fix tf doc generation script for local runs Merged PR 92
    updates the tf autogen file check to:
    - just look for current changes
    - flag only auto-gen files
    update autogen to support running script form multiple locations
    
    Related work items: #589

--- src/update-all-terraform-docs.sh
+++ src/update-all-terraform-docs.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-# Find all top-level directories in the src folder that contain a nested terraform folder
-echo "$PWD"
-TERRAFORM_DOCS_CONFIG=".terraform-docs.yml"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TERRAFORM_DOCS_CONFIG="${TERRAFORM_DOCS_CONFIG:-$SCRIPT_DIR/../.terraform-docs.yml}"
 echo "Starting document processing using terraform-docs with config file: $TERRAFORM_DOCS_CONFIG"
-
-echo "Searching top-level directories containing terraform ..."
+echo
 echo "Starting terraform doc updates ..."
+echo
 
 # Loop over top_level_dirs and select only the folders 
 # where there is a "terraform" subfolder and call tf-docs on them
-find src -mindepth 1 -maxdepth 1 -type d | while read -r folder; do
+find "SCRIPT_DIR/../src" -mindepth 1 -maxdepth 1 -type d | while read -r folder; do
   if [ -d "$folder/terraform" ]; then
     echo "Updating Terraform docs in folder: $folder/terraform"
     terraform-docs "$folder/terraform" --config "$TERRAFORM_DOCS_CONFIG"
     echo "Completed processing Terraform docs in folder: $folder"
+    echo
   fi
 done
 
