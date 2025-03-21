038a495fdd214cf58940751f1225f25e9483b5f6
Allen Greaves <allengreaves@microsoft.com>
2025-03-21 22:31:36 +0000

    Merged PR 213: fix: point opc-plc-deployment.yaml to latest sha to prevent breaking changes
    - _fix_(**iot-ops**): Update opc-plc-deployment.yaml to use latest sha from [explore-iot-operations](https://github.com/Azure-Samples/explore-iot-operations) repo to prevent breaking changes.
    
    Related work items: #807

--- src/040-iot-ops/scripts/apply-simulator.sh
+++ src/040-iot-ops/scripts/apply-simulator.sh
@@ -9,7 +9,10 @@ fi
 # Set error handling to continue on errors
 set +e
 
-until kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/explore-iot-operations/main/samples/quickstarts/opc-plc-deployment.yaml --kubeconfig "$kube_config_file"; do
+# Deploy the current latest opc-plc-deployment.yaml from https://github.com/Azure-Samples/explore-iot-operations.
+# This is to prevent breaking changes from the explore-iot-operations repo impacting this repo.
+# To update to the latest version, a new hard-link to a specific sha will be required after full testing and verification.
+until kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/explore-iot-operations/b11625fc6314a0ee4d61ab7bbcc9e9315c139dab/samples/quickstarts/opc-plc-deployment.yaml --kubeconfig "$kube_config_file"; do
   echo "Error applying, retrying in 5 seconds"
   sleep 5
 done
