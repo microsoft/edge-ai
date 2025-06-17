5e3d2320151b3e4f389a58d547cbdf524984bfcc
Marcel Bindseil <marcbind@microsoft.com>
2025-06-17 23:05:40 +0100

    refactor(scripts): update GitHub API calls to use dynamic repository paths
    - Modify access token generation to accept repository as a parameter
    - Update PR creation script to use dynamic repository path
    
    Signed-off-by: Marcel Bindseil <marcbind@microsoft.com>

--- scripts/github/access-tokens-url.sh
+++ scripts/github/access-tokens-url.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 JWT=$1
+REPO=$2
 response=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $JWT" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/commercial-software-engineering/Iac-for-the-Edge/installation)
+  https://api.github.com/repos/$REPO/installation)
 
 access_tokens_url=$(echo "$response" | jq -r '.access_tokens_url')
 echo "$access_tokens_url"
--- scripts/github/create-pr.sh
+++ scripts/github/create-pr.sh
@@ -2,11 +2,12 @@
 TOKEN=$1
 BRANCH=$2
 COMMITMSG=$3
+REPO=$2
 
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/commercial-software-engineering/Iac-for-the-Edge/pulls \
+  https://api.github.com/repos/$REPO/pulls \
   -d "{\"title\":\"AzDO merge for branch $BRANCH\",\"body\":\"Sync from AzDO - IaC for the Edge repo having the following changes: $COMMITMSG\",\"head\":\"$BRANCH\",\"base\":\"main\"}"
