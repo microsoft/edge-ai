2de3b461c862eba7e8b9c8cca4450c0cd1b02c07
Marcel Bindseil <marcbind@microsoft.com>
2025-06-18 18:00:00 +0100

    refactor(scripts): improve PR creation script readability
    - Add echo statement for repo path
    - Ensure consistent quoting in curl command
    
    Signed-off-by: Marcel Bindseil <marcbind@microsoft.com>

--- scripts/github/create-pr.sh
+++ scripts/github/create-pr.sh
@@ -9,5 +9,5 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$REPO/pulls \
+  "https://api.github.com/repos/$REPO/pulls" \
   -d "{\"title\":\"AzDO merge for branch $BRANCH\",\"body\":\"Sync from AzDO - IaC for the Edge repo having the following changes: $COMMITMSG\",\"head\":\"$BRANCH\",\"base\":\"main\"}"
