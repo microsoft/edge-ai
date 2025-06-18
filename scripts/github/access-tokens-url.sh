7189c1cf012696771959c984f9194ec4224e8c10
Marcel Bindseil <marcbind@microsoft.com>
2025-06-18 12:21:34 +0100

    refactor(scripts): simplify URL assignment for access tokens retrieval
    - Store the URL in a variable for clarity
    - Log the URL before making the request
    
    Signed-off-by: Marcel Bindseil <marcbind@microsoft.com>

--- scripts/github/access-tokens-url.sh
+++ scripts/github/access-tokens-url.sh
@@ -2,11 +2,13 @@
 JWT=$1
 REPO=$2
 echo "Fetching access tokens URL for repository: $REPO"
+url="https://api.github.com/repos/$REPO/installation"
+echo "url: $url"
 response=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $JWT" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/$REPO/installation")
+  "$url")
 
 access_tokens_url=$(echo "$response" | jq -r '.access_tokens_url')
 echo "Access Tokens URL: $access_tokens_url"
