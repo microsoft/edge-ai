40eeec2d8a98bdbcb891f0b78ab63543624bf3f3
Marcel Bindseil <marcbind@microsoft.com>
2025-06-24 11:10:13 +0200

    chore(terraform): remove unused variables for Azure Container Registry
    - Delete variables related to Azure Container Registry
    - Clean up variable definitions for better clarity
    
    Signed-off-by: Marcel Bindseil <marcbind@microsoft.com>

--- src/000-cloud/070-kubernetes/terraform/variables.tf
+++ src/000-cloud/070-kubernetes/terraform/variables.tf
@@ -8,22 +8,10 @@ variable "should_create_aks" {
   default     = false
 }
 
-variable "should_create_acr_private_endpoint" {
-  type        = bool
-  description = "Should create a private endpoint for the Azure Container Registry. Default is false."
-  default     = false
-}
-
 variable "should_create_arc_cluster_instance" {
   type        = bool
   description = "Should create an Azure Arc Cluster Instance. Default is false."
   default     = false
-}
-
-variable "sku" {
-  type        = string
-  description = "SKU for the Azure Container Registry. Options are Basic, Standard, Premium. Default is Premium because of the need for private endpoints."
-  default     = "Premium"
 }
 
 variable "node_count" {
